36adcd669bfcc57dc5e5be0d503eeb49c901329e
Alan Shiau <alanshiauwork@hotmail.com>
2020-10-23 23:35:18 +1100

    added new data

---
+++ data_wranging/crime_by_time.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+import os
+
+def bin_assign(x):
+    # print(x)
+    return 1
+df = pd.read_csv("SF Visulisation/Police_Department_Incidents_-_Previous_Year__2016_.csv")
+
+
+
+
+
+
+
+df["Time"] = df["Time"].apply(lambda x: x.split(":")[0])
+df = df.groupby(['Category', 'Time', "PdDistrict"]).count()
+
+
+df.reset_index(inplace = True)
+df.rename(columns = {"IncidntNum": "Count"}, inplace = True)
+df = df[["Category", "Time", "PdDistrict", "Count"]]
+
+df.to_csv("SF Visulisation/crime_by_time.csv")
+
+
